a18a2baf38710fffb41882964959a0d8e98cd597
alexxGV <63457687+alexxGV@users.noreply.github.com>
2021-02-22 21:42:08 +0100

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALEXFLIX.Models
+{
+    public class Persona
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public String Descripcion { get; set; }
+        public int Valoracion { get; set; }
+    }
+}
